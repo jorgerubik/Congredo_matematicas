8f5e15da48c0d3a519e693b81875decb78411b82
jorgerubik <jorge_delux_123@hotmail.com>
2013-10-12 13:35:24 -0500

    Termino html de registro de trabajos :D

--- content/RegistroTrabajos.php
+++ content/RegistroTrabajos.php
@@ -1,7 +1,9 @@
 <h1 align="justify"> El registro de trabajos ser&aacute; liberado a partir del d&iacute;a 21 de Octubre del 2013</h1>
 		<form action="registro_ponencia.php" method="post" autocomplete="off" class="forms">
 			<button type="submit" name="ponencia" id="boton">Ponencia</button>  <br>
+			<button type="submit" name="cartel" id="boton">Cartel</button><br>
+		</form>	
+		<form action="registro_taller.php" method="post" autocomplete="off" class="forms"> 
+			<button type="submit" name="taller" id="boton">Taller</button><br>
+			<button type="submit" name="curso" id="boton">Curso</button><br>
 		</form>
-		<button type="button" name="cartel" id="boton">Cartel</button><br>
-		<button type="button" name="taller" id="boton">Taller</button><br>
-		<button type="button" name="curso" id="boton">Curso</button><br>
